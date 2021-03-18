630b4e648296818a8d043dcdb4fe3f11dc8dd839
Tamires Morais <tamires.mgaldino@uni9.edu.br>
2021-03-18 13:27:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    int a, b;
+     for(a = 0; a <= 10; a++) {
+        for(b = 0;b <= 10; b++) {
+          System.out.println(a + " x " + b + " = " + a * b);
+        }
+      }
+  } 
+}
